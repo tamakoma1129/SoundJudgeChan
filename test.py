853eecf9fb9cac5f8087fa98197b16bb3da39042
tamakoma <tamakoma0125@gmail.com>
2023-06-27 15:03:46 +0900

    VSCodeからのコミットテスト

--- test.py
+++ test.py
@@ -1,2 +1,21 @@
-s = 1
-print(s)
+import tkinter as tk
+from tkinterdnd2 import DND_FILES, TkinterDnD
+
+def drop(event):
+    filepath = event.data
+    print(f'{filepath} was dropped.')
+    # ここでscikit-learnを用いた音源分析などを実装します。
+
+root = TkinterDnD.Tk()
+root.withdraw()  # tkinterのウィンドウを一時的に隠す
+
+frame = tk.Frame(root, name='drag_and_drop_frame', width=300, height=300)
+frame.pack()
+
+# フレームにドロップ操作をバインド
+frame.drop_target_register(DND_FILES)
+frame.dnd_bind('<<Drop>>', drop)
+
+root.update()
+root.deiconify()  # tkinterのウィンドウを再表示
+root.mainloop()
